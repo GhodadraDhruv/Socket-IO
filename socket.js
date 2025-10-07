3467bb533977816e0d9b575572b08f8c735c21de
Dhruv Ghodadra <164461706+GhodadraDhruv@users.noreply.github.com>
2025-10-07 18:35:38 +0530

    Add files via upload

---
+++ socket.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const app=express();
+const PORT=3000;
+const http=require('http');
+const server=http.createServer(app);
+const {Server}=require('socket.io');
+const io=new Server(server);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+io.on("connected",(socket)=>{
+    console.log("a User Connected ...");
+    socket.on("chat",(msg)=>{
+        io.emit("chat",msg);
+    }),
+    socket.on("disconnected",(socket)=>{
+        console.log("User Disconnected ...");
+    });
+});
+
+server.listen(PORT,()=>{
+    console.log(`Server Listenning On http://localhost:${PORT}`);
+});
